a9fca11858b83a2e816e77c52218218fb259d605
songhj8304 <127584828+songhj8304@users.noreply.github.com>
2024-08-20 13:22:49 +0900

    java time ex commit

---
+++ timeEx/TestAdjusters.java
@@ -0,0 +1,22 @@
+package time.timeEx;
+
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class TestAdjusters {
+
+    public static void main(String[] args) {
+        //입력 받은 월의 첫날 요일과 마지막날 요일을 구해라.
+        int year = 2024;
+        int month = 8;
+
+        LocalDate date = LocalDate.of(year,month,1);
+        DayOfWeek firstDayOfWeek = date.getDayOfWeek();
+        DayOfWeek lastDayOfWeek = date.with(TemporalAdjusters.lastDayOfMonth()).getDayOfWeek();
+        System.out.println("firstDayOfWeek = " + firstDayOfWeek);
+        System.out.println("lastDayOfWeek = " + lastDayOfWeek);
+
+    }
+}
